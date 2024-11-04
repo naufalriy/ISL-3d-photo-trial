c2e1e1e4433b45ae526872adf5d0c2216726d85d
naufalriy <169055766+naufalriy@users.noreply.github.com>
2024-11-04 12:33:35 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,466 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-front-edited",
+      "name": "1 FRONT EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5340657994380251,
+          "pitch": -0.025350059400132352,
+          "rotation": 6.283185307179586,
+          "target": "1-2-front-wwt-and-to-hp-edited"
+        },
+        {
+          "yaw": -0.020060151819750516,
+          "pitch": 0.04528832952916595,
+          "rotation": 7.0685834705770345,
+          "target": "5-6-upper-edited"
+        },
+        {
+          "yaw": 2.4386142434264686,
+          "pitch": 0.23013321080816596,
+          "rotation": 0,
+          "target": "7-8-parking-area-edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1686430476851717,
+          "pitch": 0.1241733178363873,
+          "title": "Main Entrance",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-2-front-wwt-and-to-hp-edited",
+      "name": "2 FRONT WWT AND TO HP EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4878678425476579,
+          "pitch": 0.11446892132289577,
+          "rotation": 0.7853981633974483,
+          "target": "2-3-to-ph-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3-to-ph-edited",
+      "name": "3 TO PH EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9626692122400726,
+          "pitch": 0.37078038360220944,
+          "rotation": 5.497787143782138,
+          "target": "4-5-tailrace-edited"
+        },
+        {
+          "yaw": 0.5353308465546611,
+          "pitch": 0.5569491210961139,
+          "rotation": 0,
+          "target": "3-4-ph-edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15409371018531104,
+          "pitch": 0.38606393695606656,
+          "title": "Power House",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-4-ph-edited",
+      "name": "4 PH EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5-tailrace-edited",
+      "name": "5 TAILRACE EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6-upper-edited",
+      "name": "6 UPPER EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8529066760882174,
+          "pitch": 0.14824490419586134,
+          "rotation": 4.71238898038469,
+          "target": "6-7-intake-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7-intake-edited",
+      "name": "7 INTAKE EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.6520769061115317,
+          "pitch": 0.690774270569225,
+          "title": "Intake",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-8-parking-area-edited",
+      "name": "8 PARKING AREA EDITED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2765752797865204,
+          "pitch": 0.1274633956587028,
+          "rotation": 0,
+          "target": "8-9-between-parking-and-office-edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2503778464547999,
+          "pitch": 0.05159093716861385,
+          "title": "Parking Area",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-9-between-parking-and-office-edited",
+      "name": "9 Between Parking and Office edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.853655048487659,
+          "pitch": 0.26637204967789785,
+          "rotation": 0.7853981633974483,
+          "target": "7-8-parking-area-edited"
+        },
+        {
+          "yaw": -0.5792267045416679,
+          "pitch": 0.08740517259300162,
+          "rotation": 0,
+          "target": "9-10-office-edited"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10-office-edited",
+      "name": "10 Office edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7698628927158602,
+          "pitch": 0.2484352583400824,
+          "rotation": 5.497787143782138,
+          "target": "10-11-wind-trubine-edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.245138843866341,
+          "pitch": -0.07583608312866374,
+          "title": "Factory",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2668814238455184,
+          "pitch": 0.04699861973456976,
+          "title": "Office",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-11-wind-trubine-edited",
+      "name": "11 Wind Trubine edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Island Seafood Ltd",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
